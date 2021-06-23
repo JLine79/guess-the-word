00df4f36e251319978ff59849d2ab8a5ac891e06
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-06-23 14:10:57 +0100

    Added check guess and win functions

--- js/script.js
+++ js/script.js
@@ -19,18 +19,15 @@ const blankLetters = function (word) {
 
 blankLetters(word);
 
-//store letter on click and clear form
+//store letter on button click and clear form
 guess.addEventListener("click", function (e) {
-    e.preventDefault();
-    let inputValue = letter.value
-    console.log(inputValue)
-    letter.value = ""
+    e.preventDefault(); //empty message text
     message.innerText = ""
-    const storedGuess = letterGuess(inputValue)
+    const storedGuess = letter.value
     if (storedGuess !== undefined) {
         makeGuess(storedGuess)
     }
-    /*console.log(storedGuess)*/
+    letter.value = ""
 })
 
 //check letter is valid
@@ -39,28 +36,69 @@ const letterGuess = function (input) {
     if (input.length === 0) {
         message.innerText = "Have a guess!"
     } 
-    
+
+//check only one letter is entered
     else if (input.length > 1) {
         message.innerText = "One letter at a time, eager beaver!"
     } 
-    
+
+//check input is a letter
     else if (!input.match(acceptedLetter)) {
         message.innerText = "Letters only please!"
     }
 
+//return letter for array
     else {
         return input 
     }
 }
 
 const makeGuess = function (letter) {
-    letter.toUpperCase()
-    if (!storedLetters.includes(letter)) {
-        storedLetters.push(letter)
+    letter = letter.toUpperCase()
+    if (storedLetters.includes(letter)) { 
+        message.innerText = "Oops! You've tried that one already." //alert letter already used
     }
 
     else {
-        message.innerText = "Oops! You've tried that one already."
+        storedLetters.push(letter)
+        showGuesses()
+        wordUpdate(storedLetters)
     }
-    console.log(storedLetters)
+}
+
+//create list of guessed letters from stored array
+const showGuesses = function () {
+    guessedLetters.innerHTML = ""
+    for (const letter of storedLetters) {
+        const newLetter = document.createElement("li")
+        newLetter.innerText = letter
+        guessedLetters.append(newLetter)
+    }
+}
+
+//update word in progress
+const wordUpdate = function (storedLetters) {
+    const wordUpper = word.toUpperCase()
+    const wordArray = wordUpper.split("")
+    /*console.log(wordArray)*/
+    const guessedWord = []
+    for (const letter of wordArray){
+        if (storedLetters.includes(letter)){
+            guessedWord.push(letter.toUpperCase())
+        }
+
+        else {
+            guessedWord.push("●")
+        }
+    }
+    wordInProgress.innerText = guessedWord.join("")
+    guessCorrect(word)
+}
+
+//check if player guessed the word
+const guessCorrect = function (word) {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win")
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats</p>`
+    }
 }
