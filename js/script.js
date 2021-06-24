618dbd62a9d2ae474b44a5ceff5457e96c5db360
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-06-24 13:40:28 +0100

    working game but want to make enter key work with play again button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const guessedLetters = document.querySelector(".guessed-letters")
-const guess = document.querySelector(".guess")
-const letter = document.querySelector(".letter")
+const guessButton = document.querySelector(".guess")
+const letterInput = document.querySelector(".letter")
 const letterForm = document.querySelector(".guess-form")
 const wordInProgress = document.querySelector(".word-in-progress")
 const remainingGuessesElement = document.querySelector(".remaining")
+const remainingGuessesSpan = document.querySelector(".remaining span") //No. guesses remaining
 const message = document.querySelector(".message")
 const playAgain = document.querySelector(".play-again")
-let word;
-const storedLetters = []
+
+let word = "magnolia"
+let storedLetters = []
 let remainingGuesses = 8
-const remainingGuessesSpan = document.querySelector(".remaining span")
 
 const getWord = async function () {
     const data = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt")
@@ -18,32 +19,31 @@ const getWord = async function () {
     const wordArray = convertData.split("\n")
     /*console.log(wordArray)*/
     const randomWord = Math.floor(Math.random() * wordArray.length)
-    word = wordArray[randomWord]
-    word.trim()
+    word = wordArray[randomWord].trim()
     blankLetters(word)
 }
 
-getWord()
+getWord() //start game
 
 //blankLetters function: Create placeholders for each letter
 const blankLetters = function (word) {
     const wordGuess = []    
-        for (let letter of word) {
+        for (const letter of word) {
         wordGuess.push("●")
     }    
     wordInProgress.innerText = wordGuess.join("")
 }
 
 //Event (button click) function: Store letter on button click and clear form
-guess.addEventListener("click", function (e) {
+guessButton.addEventListener("click", function (e) {
     e.preventDefault(); //empty message text
     message.innerText = "";
-    const storedGuess = letter.value; //letter entered by player
+    const storedGuess = letterInput.value; //letter entered by player
     const validGuess = letterGuess(storedGuess); //check input is a letter
     if (validGuess) {
         makeGuess(storedGuess)
     }
-    letter.value = "";
+    letterInput.value = "";
     letterForm.reset();
 });
 
@@ -72,7 +72,6 @@ const letterGuess = function (input) {
 
 //makeGuess function: Submit valid letter and check against word
 const makeGuess = function (letter) {
-    const letterField = document.querySelector(".guess-form")
     letter = letter.toUpperCase()
     if (storedLetters.includes(letter)) { 
         message.innerText = "Oops! You've tried that one already." //alert letter already used
@@ -84,7 +83,6 @@ const makeGuess = function (letter) {
         letterCountdown(letter) //reduce number of guesses remaining if letter is wrong
         wordUpdate(storedLetters) //add correct letter to placeholder in word
     }
-    letterField.reset() //reset letter entry box
 }
 
 //create list of guessed letters from stored array
@@ -98,54 +96,84 @@ const showGuesses = function () {
 }
 
 //update word with correctly guessed letters
-    const wordUpdate = function (storedLetters) {
-        word = word.toUpperCase()
-        const wordArray = word.split("")
-        const guessedWord = []
-            for (const letter of wordArray){
-                if (storedLetters.includes(letter)){
-                    guessedWord.push(letter.toUpperCase())
-                }
-
-            else {
-                guessedWord.push("●")
+const wordUpdate = function (storedLetters) {
+    wordUpper = word.toUpperCase()
+    const wordArray = wordUpper.split("")
+    const guessedWord = []
+        for (const letter of wordArray){
+            if (storedLetters.includes(letter)){
+                guessedWord.push(letter.toUpperCase())
             }
+
+        else {
+            guessedWord.push("●")
+        }
     }
-    wordInProgress.innerText = guessedWord.join("")
-    guessCorrect(word)
+wordInProgress.innerText = guessedWord.join("")
+guessCorrect()
 }
 
 //Count number of guesses
 const letterCountdown = function (guess){
-    const upperWord = guess.toUpperCase()
-    if (word.includes(upperWord)) {
-        message.innerText = "You got one! Keep guessing!"
-    }
-
-    else {
+    const upperWord = word.toUpperCase()
+    if (!upperWord.includes(guess)) {
         message.innerText = "Nope! Try again!"
         remainingGuesses -= 1
     }
 
+    else {
+        message.innerText = "You got one! Keep guessing!"
+    }
+
     if (remainingGuesses === 0) {
-        message.innerText = `Sorry, you've run out of guesses. The word was ${word}`
+        message.innerHTML = `Sorry, you've run out of guesses. The word was ${word}`
         remainingGuessesElement.innerText = ""
+        startOver()
     }
 
-    if (remainingGuesses === 1) {
-        remainingGuessesSpan.innerText = "1 guess"
+    else if (remainingGuesses === 1) {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`
     }
 
-    if (remainingGuesses > 1) {
+    else {
         remainingGuessesSpan.innerText = `${remainingGuesses} guesses`
     }
 }
 
 //check if player guessed the word
-const guessCorrect = function (word) {
+const guessCorrect = function () {
     if (word.toUpperCase() === wordInProgress.innerText) {
         message.classList.add("win")
-        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats</p>`
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`
+        
+        startOver()
     }
 }
 
+//Start game again
+const startOver = function () {
+    guessButton.classList.add("hide")
+    remainingGuessesElement.classList.add("hide")
+    guessedLetters.classList.add("hide")
+    playAgain.classList.remove("hide")   
+}
+
+//Play again click event
+playAgain.addEventListener("click", function () {
+    //reset & add new word
+    message.classList.remove("win")
+    message.innerText = ""
+    remainingGuessesElement.innerHTML = ""
+    guessedLetters.innerHTML = ""
+    wordInProgress.innerHTML = ""
+    remainingGuesses = 8
+    storedLetters = []
+    remainingGuessesSpan.innerText = `${remainingGuesses} guesses`
+    
+    getWord()
+
+    guessButton.classList.remove("hide")
+    remainingGuessesElement.classList.remove("hide")
+    guessedLetters.classList.remove("hide")
+    playAgain.classList.add("hide")
+})
